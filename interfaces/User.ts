bd7ae0091742a83dd11c38048e1a7972e54b2da1
JulienMounianman <julienmounianman@gmail.com>
2017-12-04 09:10:09 +0100

    test

--- interfaces/User.ts
+++ interfaces/User.ts
@@ -1,7 +1,11 @@
+export enum State { Pending, Activated};
+
+
 export class User {
     username: string;
     password: string;
     email: string;
     firstname: string;
     lastname: string;
+    state : State;
 }
