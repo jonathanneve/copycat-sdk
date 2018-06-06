aeaf37a76e71aa9f679997a89e3ea812a8c94c24
JulienMounianman <julienmounianman@gmail.com>
2018-06-06 12:59:36 +0200

    ok

--- interfaces/listener.ts
+++ interfaces/listener.ts
@@ -1,4 +1,7 @@
-export interface listener {
-    tableName : string
-    fieldName : string 
+export interface ListenerTableDef {
+    fieldNames : string[]
+}
+
+export interface Listener {
+    tableNames: ListenerTableDef[];
 }
